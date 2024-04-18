a5a755d799b13c4d5903ff6dca77d3c82aee10c7
Miguel Ortiz <ortizmichel390@gmail.com>
2024-04-18 02:09:56 -0300

    Improvements in routing and user registration

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,16 @@ import TableConfiguration from "./componentes/TableConfiguration";
 import NumberTable from "./componentes/NumberTable";
 import UserNavbar from "./componentes/NavbarUser";
 import UsersList from "./componentes/UsersList";
+import UserRegistrationForm from "./componentes/UserRegistrationForm";
 
 const App: React.FC = () => {
   return (
     <Router>
       <UserNavbar />
       <Routes>
-        <Route path="/" element={<NumberTable />} />
+        <Route path="/" element={<UserRegistrationForm />} />
         <Route path="/config" element={<TableConfiguration />} />
+        <Route path="/numbers" element={<NumberTable />} />
         <Route path="/users" element={<UsersList />} />
       </Routes>
     </Router>
