75eb0783a6d3abde3989828dd6d7eea9583568cf
Miguel Ortiz <ortizmichel390@gmail.com>
2024-06-04 19:10:00 -0300

    Add icon images to the footer and update HomePage component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,6 +5,10 @@ import {
   SparklesIcon,
 } from "@heroicons/react/24/solid";
 import easyCounterLogo from "../../public/images/easy-counter-logo.png";
+import githubIcon from "../../public/images/github.png";
+import linkedinIcon from "../../public/images/linkedin.png";
+import gmailIcon from "../../public/images/gmail.png";
+import whatsappIcon from "../../public/images/whatsapp.png";
 
 function HomePage() {
   return (
@@ -120,6 +124,81 @@ function HomePage() {
           números. Ya sea para rifas, eventos, clubes, o programas de lealtad,
           EasyCOUNTER es tu solución perfecta.
         </p>
+      </div>
+
+      <div className="w-full mt-8 p-1 bg-gray-300 rounded-lg border-t-2 border-purple-500 flex items-center">
+        <p className="text-xs mr-4 ml-10">
+          Miguel Ortiz, Junior FullStack Developer web. Año 2024
+          <br />
+          Si deseas conocer más sobre mi trabajo o contactar conmigo, aquí
+          tienes mis enlaces de contacto:
+        </p>
+        <div className="flex ml-5">
+          <div className="flex flex-col items-center mr-10">
+            <a
+              href="https://github.com/mikiortiz"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={githubIcon}
+                alt="GitHub"
+                className="h-8 w-8 mt-2 mb-1 animate-bounce"
+              />
+            </a>
+            <span className="text-xs text-purple-700 hover:underline font-semibold">
+              GitHub
+            </span>
+          </div>
+          <div className="flex flex-col items-center ml-10">
+            <a
+              href="https://www.linkedin.com/in/miguel-ortiz-9736b32a5/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={linkedinIcon}
+                alt="LinkedIn"
+                className="h-8 w-8 mt-2 mb-1 animate-bounce"
+              />
+            </a>
+            <span className="text-xs text-purple-700 hover:underline font-semibold">
+              LinkedIn
+            </span>
+          </div>
+          <div className="flex flex-col items-center ml-20">
+            <a
+              href="mailto:ortizmichel390@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={gmailIcon}
+                alt="Gmail"
+                className="h-8 w-8 mt-2 mb-1 animate-bounce"
+              />
+            </a>
+            <span className="text-xs text-purple-700 hover:underline font-semibold">
+              Correo
+            </span>
+          </div>
+          <div className="flex flex-col items-center ml-20">
+            <a
+              href="https://wa.me/?text=Hola,%20soy%20Miguel%20Ortiz%20Junior%20FullStack%20Developer%20web.%20Envíame%20un%20mensaje%20para%20contactarme."
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={whatsappIcon}
+                alt="WhatsApp"
+                className="h-8 w-8 mt-2 mb-1 animate-bounce"
+              />
+            </a>
+            <span className="text-xs text-purple-700 hover:underline font-semibold">
+              WhatsApp
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   );
